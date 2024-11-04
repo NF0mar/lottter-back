c50bcce4ae353a0e6da591b8f1b793b97ed4ecd5
Nur Farah <nuurfaarax15@gmail.com>
2024-11-04 20:43:25 +0300

    environment variable corrected

--- index.js
+++ index.js
@@ -14,11 +14,12 @@ app.use(cors());
 app.use(express.json());
 
 // Database Connection
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log('Connected to MongoDB'))
 .catch((err) => console.error('Failed to connect to MongoDB:', err));
+console.log('MongoDB URI:', process.env.MONGODB_URI);
 
 // Routes
 app.use('/api/users', userRoutes);
